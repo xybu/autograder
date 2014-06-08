97615d67632d2f59fa7878656a72abb685111ed1
Xiangyu Bu <xybu92@live.com>
2014-06-07 20:39:22 -0400

    Some minor script changes

--- inst.sh
+++ inst.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-sudo apt-get install pip
+# Install packages for the server component
+sudo apt-get install python-software-properties
+sudo add-apt-repository ppa:nginx/stable
+sudo add-apt-repository ppa:ondrej/php5
 
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install nginx-extras mysql-client mysql-server php5-common php5-fpm php5-dev php5-mysql php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xsl php5-apcu
+sudo apt-get install python-pip
 
 cd web
 mkdir -p tmp/cache
-mkdir -p tmp/upload
+chmod 775 tmp
+chmod 755 tmp/cache
+#mkdir -p tmp/upload
