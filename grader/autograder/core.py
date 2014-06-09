cba4ef2873e69c4582c65b79fb472875c19c6c98
Xiangyu Bu <xybu92@live.com>
2014-06-09 00:31:18 -0400

    Some UnitTest module code

--- grader/autograder/core.py
+++ grader/autograder/core.py
@@ -1,11 +1,27 @@
 #!/usr/bin/python
 
-# Autograder Core Class
-# Provides the API and engine of autograder program
+import unittest
 
-class AutoGrader:
+class GraderTestSuite:
 	def __init__(self):
-		pass
+		pass	
 
 class HandlerFactory:
+	pass
+
+class GraderResult(unittest.TextTestResult):
+"""A Python UnitTest test runner implementation"""
+
+	def __init__(self, stream=sys.stderr, descriptions=1, verbosity=1):
+		unittest.TextTestResult.__init__(self, stream, descriptions, verbosity)
+	
+	def startTest(self, test):
+		"""The function is called every time when the test case test is about to run."""
+		pass
+	
+	def stopTest(self, test):
+		"""The function is called every time when the test case is done."""
+		pass
+	
+class GraderRunner:
 	pass
