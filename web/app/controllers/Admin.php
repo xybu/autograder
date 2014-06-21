5b8c493697c1470cb2b960151917fb8d503d5719
Xiangyu Bu <xybu92@live.com>
2014-06-21 19:17:05 -0400

    Stubs for admin panel

--- web/app/controllers/Admin.php
+++ web/app/controllers/Admin.php
@@ -1,8 +1,18 @@
 <?php
 
+namespace controllers;
+
 class Admin extends \Controller {
 
 	function showAdminHomepage($base) {
+		$user_info = $this->getUserStatus();
+		if ($user_info == null)
+			$this->json_echo(array("error" => "not_logged_in", "error_description" => "You need to log in to perform the request."), true);
+		
+		if (!$user_info["role"]["permissions"]["manage"])
+			$this->json_echo(array("error" => "permission_denied", "error_description" => "You cannot access admin panel."), true);
+			
+		$this->setView("admincp.html");
 	}
 
 }
