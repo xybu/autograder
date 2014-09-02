f2091951fdb4ac951d70d2b9333c9eff40bc3eaa
Xiangyu Bu <xybu92@live.com>
2014-09-01 20:06:35 -0400

    Update setup.sh
    Install cgroup-bin

--- setup.sh
+++ setup.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 # install packages for the server component
-sudo apt-get install python-software-properties
+sudo apt-get -y install python-software-properties
 sudo add-apt-repository ppa:nginx/stable
 sudo add-apt-repository ppa:ondrej/php5
 
 sudo apt-get update && sudo apt-get upgrade
 sudo apt-get install nginx-extras mysql-client mysql-server python-mysql.connector php5-common php5-fpm php5-dev php5-mysql php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xsl php5-apcu
 
-sudo apt-get -y install python-pip
+sudo apt-get -y install python-pip build-essential cgroup-bin
+# mount -t cgroup none /sys/fs/cgroup
 
 # make log directory
 mkdir log
